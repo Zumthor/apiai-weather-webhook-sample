f8f9653f356c233dd0e1fa314d5f92829536bf81
Zumthor <edmund.procter@gmail.com>
2017-05-09 09:18:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,18 +45,19 @@ def processRequest(req):
     
     elif req.get("result").get("action") == "StateCap":
         #read csv
-        import csv
-        with open(r'C:\Users\Edmund.Procter\Desktop\State_Capitals.csv', 'r') as f:
-            raw = csv.DictReader(f)
-            states = []
-            capitals = []
-            for row in raw:
-                state = row['State']
-                capital = row['Capital']
+#         import csv
+#         with open(r'C:\Users\Edmund.Procter\Desktop\State_Capitals.csv', 'r') as f:
+#             raw = csv.DictReader(f)
+#             states = []
+#             capitals = []
+#             for row in raw:
+#                 state = row['State']
+#                 capital = row['Capital']
 
-                states.append(state)
-                capitals.append(capital)
-
+#                 states.append(state)
+#                 capitals.append(capital)
+        states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+        capitals = ['Montgomery', 'Juneau', 'Phoenix', 'Little Rock', 'Sacramento', 'Denver', 'Hartford', 'Dover', 'Tallahassee', 'Atlanta', 'Honolulu', 'Boise', 'Springfield', 'Indianapolis', 'Des Moines', 'Topeka', 'Frankfort', 'Baton Rouge', 'Augusta', 'Annapolis', 'Boston', 'Lansing', 'Saint Paul', 'Jackson', 'Jefferson City', 'Helena', 'Lincoln', 'Carson City', 'Concord', 'Trenton', 'Santa Fe', 'Albany', 'Raleigh', 'Bismarck', 'Columbus', 'Oklahoma City', 'Salem', 'Harrisburg', 'Providence', 'Columbia', 'Pierre', 'Nashville', 'Austin', 'Salt Lake City', 'Montpelier', 'Richmond', 'Olympia', 'Charleston', 'Madison', 'Cheyenne']
         whatSt = req.get("result").get("parameters").get("geo-state-us")
         capdex = states.index(whatSt)
         theCap = capitals[capdex]
