461a660d8f936cee87fbb9de9c6041f5cdcf5135
Zumthor <edmund.procter@gmail.com>
2017-05-09 09:29:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,12 +56,16 @@ def processRequest(req):
 
 #                 states.append(state)
 #                 capitals.append(capital)
+        #data
         states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
         capitals = ['Montgomery', 'Juneau', 'Phoenix', 'Little Rock', 'Sacramento', 'Denver', 'Hartford', 'Dover', 'Tallahassee', 'Atlanta', 'Honolulu', 'Boise', 'Springfield', 'Indianapolis', 'Des Moines', 'Topeka', 'Frankfort', 'Baton Rouge', 'Augusta', 'Annapolis', 'Boston', 'Lansing', 'Saint Paul', 'Jackson', 'Jefferson City', 'Helena', 'Lincoln', 'Carson City', 'Concord', 'Trenton', 'Santa Fe', 'Albany', 'Raleigh', 'Bismarck', 'Columbus', 'Oklahoma City', 'Salem', 'Harrisburg', 'Providence', 'Columbia', 'Pierre', 'Nashville', 'Austin', 'Salt Lake City', 'Montpelier', 'Richmond', 'Olympia', 'Charleston', 'Madison', 'Cheyenne']
-        whatSt = req.get("result").get("parameters").get("geo-state-us")
-        capdex = states.index(whatSt)
-        theCap = capitals[capdex]
-        speech = "The capital of " + whatSt,"is: " + theCap
+        
+        result = req.get("result")
+        parameters = result.get("parameters")
+        state = parameters.get("geo-state-us")
+        capdex = states.index(state)
+        capital = capitals[capdex]
+        speech = "The capital of " + state,"is: " + capital
     
     else:
         speech = "action call not recognised"
