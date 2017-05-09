39df3fa3350c74d641914fa054dc217508286995
Zumthor <edmund.procter@gmail.com>
2017-05-09 09:33:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,8 @@ def processRequest(req):
         state = parameters.get("geo-state-us")
         capdex = states.index(state)
         capital = capitals[capdex]
-        speech = "The capital of " + state,"is: " + capital
+        #speech = "The capital of " + state,"is: " + capital
+        speech = "test: " + state
     
     else:
         speech = "action call not recognised"
